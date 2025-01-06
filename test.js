7dbde73160723199a9c8e5dd3988adfc41ac0ff0
Renée Kooi <renee@kooi.me>
2025-01-06 13:26:25 +0100

    Avoid import.meta.resolve

--- test.js
+++ test.js
@@ -1,13 +1,10 @@
 import test from 'node:test'
 import assert from 'node:assert'
 import fs from 'node:fs'
-import { fileURLToPath } from 'node:url'
 import docpt from 'prop-types-table'
 
 test('class propTypes', function () {
-  const source = fs.readFileSync(
-    fileURLToPath(import.meta.resolve('@u-wave/react-youtube/src/index.js'))
-  )
+  const source = fs.readFileSync('./node_modules/@u-wave/react-youtube/src/index.js')
   const expected = `
 | Name | Type | Default | Description |
 |:-----|:-----|:-----|:-----|
